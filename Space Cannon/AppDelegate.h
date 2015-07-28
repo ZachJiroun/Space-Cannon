221848c6b21940f409f172788851d48d02e8d3c6
ZachJiroun <zjiroun@gmail.com>
2015-07-27 21:27:46 -0400

    Initial Commit

---
+++ Space Cannon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Space Cannon
+//
+//  Created by Zach Jiroun on 7/27/15.
+//  Copyright (c) 2015 Zach Jiroun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
